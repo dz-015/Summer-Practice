f26de483c1596f1bcdf6a2f6303ea178735c5ff8
Arseny <killhamut@gmail.com>
2022-07-19 02:59:37 +0300

    [CHANGE]directing vector is now indeed vector

--- line.py
+++ line.py
@@ -1,6 +1,7 @@
 from point import Point
+from vector import Vector
 
 class Line():
     def __init__(self, left_point:Point, right_point:Point):
-        self.directing_vector = right_point - left_point
+        self.directing_vector:Vector = right_point - left_point
         self.point = left_point
