a537fd374fa6da02cfd278bcc776ec3363363d3b
Arseny <killhamut@gmail.com>
2022-07-19 02:55:08 +0300

    [ADD]Added new class Vector

---
+++ vector.py
@@ -0,0 +1,10 @@
+class Vector():
+    """
+    Provides 3-dimensional vector
+    """
+    def __init__(self, x:float=0, y:float=0, z:float=0):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    
