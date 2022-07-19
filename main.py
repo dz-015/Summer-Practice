55ef1226a8a0e454d1827af5ca7a36a3edda2463
Arseny <killhamut@gmail.com>
2022-07-19 04:21:20 +0300

    [CHANGE]main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import calculations
+from cylindrical_point import CylindricalPoint
+
 def main():
-    pass
+    p1 = CylindricalPoint(0, 0, 0)
+    p2 = CylindricalPoint(0, 1, 1)
+    p3 = CylindricalPoint(1, 0, 1)
+    print(calculations.triangle_area(
+        p1.convert(),
+        p2.convert(),
+        p3.convert()
+    ))
 
 if __name__ == "__main__":
     main()
