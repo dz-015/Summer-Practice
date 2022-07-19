81afe0eaaaa09ca32427710a98031ec5b64d1b7a
Arseny <killhamut@gmail.com>
2022-07-19 03:12:23 +0300

    [ADD]Added vector mult to vector class

--- vector.py
+++ vector.py
@@ -8,6 +8,14 @@ class Vector():
         self.x = x
         self.y = y
         self.z = z
+
+    def __mult__(self, another_vector):
+        """
+        provides vector multiplication
+        """
+        return Vector((self.y * another_vector.z) - (self.z * another_vector.y),
+                     (self.z * another_vector.x) - (self.x * another_vector.z),
+                     (self.x * another_vector.y) - (self.y * another_vector.x))
     
     def magnitude(self):
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
